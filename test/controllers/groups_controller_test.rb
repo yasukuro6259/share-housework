3c559cf840842d34782b057a74a7e145de36da8e
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-10-10 08:13:40 +0900

    groupscontoller及びviewファイルの作成

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class GroupsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get groups_index_url
+    assert_response :success
+  end
+
 end
