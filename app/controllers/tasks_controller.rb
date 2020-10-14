b9a9a5eb0a8f453d48ca4d3bc8e429b9d20b85dd
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-10-14 16:46:28 +0900

    tasksコントローラーファイルの記述、タスク作成ページの作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+  def index
+    @task = Task.new
+    @group = Group.find(params[:group_id])
+    @tasks = @group.tasks.includes[:user]
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  private
+  def task_params
+    parameter.require(:task).permit(:content, :description, :image).merge(user_id: current_user.id)
+  end
 end
