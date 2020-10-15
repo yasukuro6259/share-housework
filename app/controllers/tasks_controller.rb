87e22343852ad760bb3618f7767ee6a77a8bc9be
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-10-15 16:48:29 +0900

    tasksコントローラファイル及びindexファイルの編集
    showアクションの設定
    詳細ページへの遷移設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+  before_action :task_params, only: :create
+
+  def index
+    @group = Group.new
+    @groups = Group.includes(:user)
+    @group_id = Group.find(params[:group_id]) #@groupだとリスト入力フォームに入力されてしまう。
+    @tasks = @group_id.tasks.includes(:user)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @task = @group.tasks.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      redirect_to new_group_task_path
+    end
+  end
+
+  def show
+    @group = Group.find(params[:group_id])
+    @task = @group.tasks.find(params[:id])
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:content, :description, :image).merge(user_id: current_user.id)
+  end
 end
