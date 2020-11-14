ba9e0d93bf69a314ab3414a1c0372034033b3c69
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-11-14 22:55:18 +0900

    ゲストログイン機能の実装
    session_controllerを自作して、クラスメソッド(guest)によってguestユーザーを登録できるよう作成した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :password, length: { minimum: 8 }
+
+  def self.guest
+    find_or_create_by!(name: 'guest', email: 'guest@gmail.com') do |user|   #find_or_create_byメソッド :存在していたら情報取得、新規なら作成・保存
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
