6e0cba685e6f8221333b72f460b7cce2cde36e45
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-11-23 09:40:39 +0900

    事前にログインさせるコードを追記した

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "groups_controller", type: :request do
   before do
-    @group = FactoryBot.create(:group)
+    user = FactoryBot.build(:user)
+    sign_in user
   end
 
   describe "GET #index" do
     it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do
       get root_path
-      binding.pry
       expect(response.status).to eq 200
     end
     it "indexアクションにリクエストすると投稿済みのリストが存在する" do
-      get root_path
+      @group = FactoryBot.build(:group)
+      get groups_path
       expect(response.body).to include @group.content
     end
   end
